305a5f44fddd3c8ee4916fa2b0a3e978659bb460
Sofia <sofiarodrigues45@gmail.com>
2022-10-21 18:31:35 -0300

    adicionando icones com expo

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native'
+import { Feather } from '@expo/vector-icons'
 
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
 
@@ -23,6 +24,10 @@ export const UserWrapper = styled.View`
     width: 100%;
 
     padding: 0 24px;
+
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
 `;
 
 export const UserInfo = styled.View`
@@ -53,4 +58,9 @@ export const UserName = styled.Text`
 
     font-size: ${RFValue(18)}px;
     font-family: ${({ theme }) => theme.fonts.bold};    
+`;
+
+export const Icon = styled(Feather)`
+    color: ${({ theme }) => theme.colors.secondary};
+    font-size: ${RFValue(24)}px;
 `;
