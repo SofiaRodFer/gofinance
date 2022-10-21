d467d6cd2a2dc63de7fbbfe7ae61fc4b2290b4ef
Sofia <sofiarodrigues45@gmail.com>
2022-10-21 19:15:52 -0300

    inserindo highlight cards no header

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components/native'
 import { Feather } from '@expo/vector-icons'
-
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+import Constants from 'expo-constants'
 
 export const Container = styled.View`
     flex: 1;
@@ -16,7 +17,7 @@ export const Header = styled.View`
     background-color: ${({ theme }) => theme.colors.primary};
 
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
     flex-direction: row;
 `;
 
@@ -24,6 +25,7 @@ export const UserWrapper = styled.View`
     width: 100%;
 
     padding: 0 24px;
+    margin-top: ${RFValue(Constants.statusBarHeight) + RFValue(28)}px;
 
     flex-direction: row;
     justify-content: space-between;
@@ -70,5 +72,8 @@ export const HighlightCards = styled.ScrollView.attrs({
     showsHorizontalScrollIndicator: false,
     contentContainerStyle: { paddingHorizontal: 24 }
 })`
+    width: 100%;
 
+    position: absolute;
+    margin-top: ${RFPercentage(20)}px;
 `;
