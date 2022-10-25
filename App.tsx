6c749ae44275851129ddc78f11a681dc0ac4da85
Sofia <sofiarodrigues45@gmail.com>
2022-10-25 19:58:46 -0300

    adicionando navegação por tabs

--- App.tsx
+++ App.tsx
@@ -10,7 +10,10 @@ import {
 } from '@expo-google-fonts/poppins'
 
 import { Register } from './src/screens/Register';
-import theme from './src/global/styles/theme'
+import theme from './src/global/styles/theme';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,7 +28,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
