dbd9be24238fa1e2f21b76b8c44bbc6c496b1fe4
Sofia <sofiarodrigues45@gmail.com>
2022-10-25 08:20:59 -0300

    adicionando botões de seleção de tipo da transação

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Input } from '../../components/Form/Input';
 import { Button } from '../../components/Form/Button';
+import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
 
 import {
     Container,
     Header,
     Title,
     Form,
-    Fields
+    Fields,
+    TransactionTypes
 } from './styles';
 
 export function Register() {
+  const [transactionType, setTransactionType] = useState('')
+
+  function handleTransactionTypeSelect(type: 'up' | 'down') {
+    setTransactionType(type)
+  }
+
   return (
     <Container>
         <Header>
@@ -21,7 +29,25 @@ export function Register() {
           <Fields>
             <Input placeholder='Nome' />
             <Input placeholder='Preço' />
+
+            <TransactionTypes>
+              <TransactionTypeButton
+                type='up'
+                title='Income'
+                onPress={() => handleTransactionTypeSelect('up')}
+                isActive={transactionType === 'up'}
+              />
+
+              <TransactionTypeButton
+                type='down'
+                title='Outcome'
+                onPress={() => handleTransactionTypeSelect('down')}
+                isActive={transactionType === 'down'}
+              />
+            </TransactionTypes>
           </Fields>
+
+
           <Button title='Enviar' />
         </Form>
     </Container>
