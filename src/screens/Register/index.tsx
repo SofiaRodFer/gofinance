7aadd2739aaf23a5e1ed05c59d8f68f8cbd3e5d6
Sofia <sofiarodrigues45@gmail.com>
2022-10-25 18:37:25 -0300

    adicionando react hook form para lidar com os dados

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Modal } from 'react-native'
+import { useForm } from 'react-hook-form';
 
+import { InputForm } from '../../components/Form/InputForm';
 import { Input } from '../../components/Form/Input';
 import { Button } from '../../components/Form/Button';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
@@ -17,6 +19,11 @@ import {
     TransactionTypes
 } from './styles';
 
+  interface FormData {
+    name: string;
+    amount: string;
+  }
+
 export function Register() {
   const [transactionType, setTransactionType] = useState('')
   const [categoryModalOpen, setCategoryModalOpen] = useState(false)
@@ -25,6 +32,11 @@ export function Register() {
     key: 'category',
     name: 'Categoria',
   })
+
+  const {
+    control,
+    handleSubmit,
+  } = useForm()
 
   function handleTransactionTypeSelect(type: 'up' | 'down') {
     setTransactionType(type)
@@ -38,6 +50,17 @@ export function Register() {
     setCategoryModalOpen(false)
   }
 
+  function handleRegister(form: FormData) {
+    const data = {
+      name: form.name,
+      amount: form.amount,
+      transactionType,
+      category: category.key
+    }
+
+    console.log(data)
+  }
+
   return (
     <Container>
         <Header>
@@ -46,8 +69,16 @@ export function Register() {
 
         <Form>
           <Fields>
-            <Input placeholder='Nome' />
-            <Input placeholder='Preço' />
+            <InputForm
+              placeholder='Nome'
+              control={control}
+              name="name"
+            />
+            <InputForm
+              placeholder='Preço'
+              control={control}
+              name="amount"
+            />
 
             <TransactionTypes>
               <TransactionTypeButton
@@ -71,8 +102,7 @@ export function Register() {
             />
           </Fields>
 
-
-          <Button title='Enviar' />
+          <Button title='Enviar' onPress={handleSubmit(handleRegister)} />
         </Form>
 
         <Modal visible={categoryModalOpen}>
